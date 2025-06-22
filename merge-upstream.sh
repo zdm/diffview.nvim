3d4b08c4df8fad7389758eeb62a38a0248f3409e
zdm <zdm@softvisio.net>
2025-06-22 22:50:26 +0300

    refactor: remove bash set -e

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+git fetch upstream || exit 1
 
-git fetch upstream
+git switch update-readme || exit 1
+git merge --no-edit upstream/main || exit 1
 
-git switch update-readme
-git merge --no-edit upstream/main
+git switch feat-msys2-support || exit 1
+git merge --no-edit upstream/main || exit 1
 
-git switch feat-msys2-support
-git merge --no-edit upstream/main
+git switch main || exit 1
+git merge --no-edit update-readme feat-msys2-support || exit 1
 
-git switch main
-git merge --no-edit update-readme feat-msys2-support
-
-git push --all
+git push --all || exit 1
